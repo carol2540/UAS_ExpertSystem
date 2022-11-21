21cdee7740ba3a511ef60fbdee4a07fe0f4aec07
OtakKentung <95601654+OtakKentung@users.noreply.github.com>
2022-11-21 07:47:29 +0700

    benerin UI

--- resources/views/user/diagnose.blade.php
+++ resources/views/user/diagnose.blade.php
@@ -6,23 +6,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../css/main.css">
     <title>Document</title>
     @yield('assets')
 </head>
 <body>
     @include("templates.navbar")
-    <div class="container">
-        <h1>Diagnose</h1>
+    <div class="container card my-4" style="height:80vh; overflow:scroll;">
+        <div class="card-header p-5">
+            <h3>Mulai Diagnosa</h3>
+        </div>
         <form method="POST" action="{{ route("cf.postmethod") }}">
             @csrf
             @foreach ($certainty as $c)
-                <h5>{{ $c['gejala'] }}</h5>
-                <input type="radio" name="{{ $c['id'] }}" value="Yes"/> Yes
-                <input type="radio" name="{{ $c['id'] }}" value="No"/> No
+                <div class="card-body">
+                    <h5>{{ $c['gejala'] }}</h5>
+                    <input class="form-checked-input" type="radio" name="{{ $c['id'] }}" value="Yes"/>
+                    <label for="{{ $c['id'] }}" class="form-checked-label">Yes</label>
+                    <input type="radio" name="{{ $c['id'] }}" value="No" class="form-checked-input"/>
+                    <label for="{{ $c['id'] }}" class="form-checked-lalbel">No</label>
+                </div>
             @endforeach
-            <br>
-            <a href="/result"><input type="submit" name="submit" value="Submit"/></a>
-            <br>
+            <a href="/result"><input  class="btn btn-primary mx-3" type="submit" name="submit" value="Submit"/></a>
         </form>
     </div>
 </body>
